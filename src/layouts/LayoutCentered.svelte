59dae8a219bc620a749ec11b94e1fc76d783ef20
Martynas Negreckis <negreckis.martynas@gmail.com>
2022-03-20 22:26:22 +0200

    Sign in screen

---
+++ src/layouts/LayoutCentered.svelte
@@ -0,0 +1,16 @@
+<style>
+    div.layout {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-items: center;
+    }
+
+    @media(min-width:768px) {
+    }
+</style>
+
+<div class="layout">
+    <slot></slot>
+</div>
